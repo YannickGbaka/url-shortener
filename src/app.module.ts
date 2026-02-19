89aa9b13e85e30584c0cc0ef1d10015ed67fbb49
Gosse Yannick Cyriaque GBAKA <gosseyannick553@gmail.com>
2026-02-19 09:28:49 +0000

    refactor(app): update TypeORM configuration and remove DatabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShortLinkModule } from './short-link/short-link.module';
 
 @Module({
   imports: [
-    DatabaseModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '9qasp5v56q8ckkf5dc.leapcellpool.com:6438',
+      host: '9qasp5v56q8ckkf5dc.leapcellpool.com',
+      port: 6438,
       username: 'mnxvjbjiwrhhihcfvmre',
       password: 'tvavnpesscuieefgvtfahhwqqbrinf',
       database: 'ppcgrmqdrpklbckjemhb',
       entities: [__dirname + '/**/*.entity{.ts}'],
       synchronize: true,
+      schema: 'myschema',
+      autoLoadEntities: true,
       ssl: true,
     }),
     ShortLinkModule,
