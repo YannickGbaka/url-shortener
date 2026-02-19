48cf06882f8c140eed6e96b0cb11b4577a421121
Gosse Yannick Cyriaque GBAKA <gosseyannick553@gmail.com>
2026-02-19 09:13:09 +0000

    feat(entity): create ShortLink entity with basic properties and decorators for URL management

--- src/short-link/entities/short-link.entity.ts
+++ src/short-link/entities/short-link.entity.ts
@@ -1,6 +1,25 @@
-import { PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export class ShortLink {
-    @PrimaryGeneratedColumn('uuid')
-    
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  @Index()
+  shortUrl: string;
+
+  @Column({ unique: true })
+  longUrl: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
