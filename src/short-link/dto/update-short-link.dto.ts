7ea22e3e9df0ad9e6b12e228f4a7fe8ca5ba869f
Gosse Yannick Cyriaque GBAKA <gosseyannick553@gmail.com>
2026-02-19 18:57:37 +0000

    fix(dto): update import for PartialType from @nestjs/swagger in UpdateShortLinkDto

--- src/short-link/dto/update-short-link.dto.ts
+++ src/short-link/dto/update-short-link.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateShortLinkDto } from './create-short-link.dto';
 
 export class UpdateShortLinkDto extends PartialType(CreateShortLinkDto) {}
