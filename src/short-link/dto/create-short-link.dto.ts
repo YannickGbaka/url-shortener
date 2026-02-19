b44182b05cfa37e227aded6d0dcc235340ab1169
Gosse Yannick Cyriaque GBAKA <gosseyannick553@gmail.com>
2026-02-19 10:30:37 +0000

    refactor(dto): update validation message for longUrl in

--- src/short-link/dto/create-short-link.dto.ts
+++ src/short-link/dto/create-short-link.dto.ts
@@ -5,7 +5,7 @@ export class CreateShortLinkDto {
     {},
     {
       message:
-        'please, provide a valid url / Veuillez svp soumettre une url valide',
+        'please, provide a valid url / Veuillez svp soumettre une url valide : https://guru-learner.com',
     },
   )
   longUrl: string;
