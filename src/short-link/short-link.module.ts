eb6eb53dbc28cfec617b8e53701a4238e659a2da
Gosse Yannick Cyriaque GBAKA <gosseyannick553@gmail.com>
2026-02-19 09:29:27 +0000

    enhance ShortLinkModule with TypeOrm integration for ShortLink entity

--- src/short-link/short-link.module.ts
+++ src/short-link/short-link.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ShortLinkService } from './short-link.service';
 import { ShortLinkController } from './short-link.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShortLink } from './entities/short-link.entity';
 
 @Module({
   controllers: [ShortLinkController],
   providers: [ShortLinkService],
+  imports: [TypeOrmModule.forFeature([ShortLink])],
 })
 export class ShortLinkModule {}
